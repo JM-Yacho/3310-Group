2382ea419d114b3f780666e10273cb3c24455d86
Christopher Nguyen <nguyen.chriss@gmail.com>
2018-04-21 18:49:33 -0500

    should be ignored

--- release.bash
+++ release.bash
@@ -1,5 +1,5 @@
 unset  SSH_ASKPASS
-export OSPL_HOME=/home/huy/Desktop/HDE/x86_64.linux
+export OSPL_HOME=/home/Desktop/HDE/x86_64.linux
 export OSPL_URI=file://ospl.xml
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${OSPL_HOME}/lib
 export PATH=${PATH}:${OSPL_HOME}/bin
