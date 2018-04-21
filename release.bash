750ab9da3af75749bd8859a6e83069a77f30b784
Christopher Nguyen <nguyen.chriss@gmail.com>
2018-04-21 18:51:57 -0500

    fixing .gitignore

--- release.bash
+++
@@ -1,5 +0,0 @@
-unset  SSH_ASKPASS
-export OSPL_HOME=/home/Desktop/HDE/x86_64.linux
-export OSPL_URI=file://ospl.xml
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${OSPL_HOME}/lib
-export PATH=${PATH}:${OSPL_HOME}/bin
